e464926cff46119f667eef7f21168798a1b87c34
Jared Weber <jaredweber54@gmail.com>
2022-11-29 19:49:19 -0600

    Added Name Ordering

--- gameHome.php
+++ gameHome.php
@@ -5,40 +5,21 @@
         <meta name="author" value="daniel palmer">
         <meta name="dateCreated" value="10/31/22">
         <link rel="stylesheet" href="styles.css">
+        <script src="calculate.js"></script>
     </head>
     <body>
-        <h1>Halo!</h1>
-        <form method="POST">
-            <input type="text" name = "text">
-            <input type="submit" name = "submit">
-        </form>
+    <button class="btn1" id="btn1" onclick="calc();"> </button>
+    <input type="text" name="text" class="text">
+
     </body>
 
      <?php
-     $array = array("Jared", "Benni", "Swags", "Trev");
-     $order = rand(0,3);
-     $i = 0;
-     echo("Order" . "<br>");
-     shuffle($array);
-    while($i < count($array)){
-     echo($array[$i] . "<br>");
-     $i++;
+
+    session_start();
+    $arr = $_SESSION['order'];
+    echo("Order:" . "<br>");
+    foreach($arr as $i){
+        echo($i . "<br>");
     }
-    echo("<br>");
-    echo($array[0] . " is up!" . "<br>");
-    $count = 0;
-    $scores = array(6, 2, 3, 4, 5, 6, 7);
-     if(isset($_POST['submit'])){
-        $text = $_POST['text'];
-        $int = (int)$text;
-        $scores[$count] = $int;
-        $i = 0;
-        $count++;
-        while($i < $count){
-        echo($scores[$i] . " ");
-        $i++;
-        }
-        
-     }
     ?> 
 </html>
--- index.php
+++ index.php
@@ -1,23 +1,53 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Login Portal</title>
+        <title>Starting Lineup</title>
         <meta name="author" value="daniel palmer">
         <meta name="dateCreated" value="10/31/22">
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        <h1>Welcome!</h1>
+        <h1>Who's Playing?</h1>
         <form method="POST">
-            <input type="submit" name="start" class="btn" value="Start Game">
+        <input type="checkbox" name="check_list[]" value="Jerry"><label>Jerry</label><br/>
+        <input type="checkbox" name="check_list[]" value="Trev"><label>Trev</label><br/>
+        <input type="checkbox" name="check_list[]" value="Mas"><label>Mas</label><br/>
+        <input type="checkbox" name="check_list[]" value="Benni"><label>Benni</label><br/>
+        <input type="checkbox" name="check_list[]" value="Swags"><label>Swags</label><br/>
+        <br>
+        <input type="submit" name="start" class="btn" value="Start Game">
         </form>
     </body>
 
+  
      <?php
-    
-     if(isset($_POST['start'])){
-         header("Location: gameHome.php");
-     }
+        session_start();
+        $db_name = "DARTS";              // Database: DARTS
+        $db_user = "jaredweber54";       // User: jaredweber54
+        $db_passwd = "web2022";  
+        $db = new mysqli("localhost", $db_user, $db_passwd, $db_name);
+
+
+        if(isset($_POST['start'])){
+            $array = array("first");
+           if(!empty($_POST['check_list'])){
+                // Loop to store and display values of individual checked checkbox.
+                 foreach($_POST['check_list'] as $selected){
+                     array_push($array, $selected);
+             }
+            } 
+            array_shift($array);    //gets rid of the "first" element
+            shuffle($array);        //reorders the array for random order
+            
+            $get_looser = "SELECT LOOSER FROM GAME ORDER BY DATE LIMIT 1";
+            $db_looser = $db->query($get_looser) or die ("failed");
+            $looser = mysqli_fetch_array($db_looser);           //gets looser of last game
+            array_push($array, $looser[0]);
+            $_SESSION['order'] = $array;
+            header("Location: gameHome.html");
+            
+        }
+     
 
     ?> 
 </html>
